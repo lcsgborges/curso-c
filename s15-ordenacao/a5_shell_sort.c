1ce75630656830cd032da3ed336fb57b10fcd5eb
lucas <lcsgborges@gmail.com>
2025-01-26 19:41:13 -0300

    feat: shell sort

---
+++ s15-ordenacao/a5_shell_sort.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <time.h>
+
+/*
+    Adaptável
+    Não estável
+    In-place
+    Complexidade: pior caso O(N^2)
+    Melhor caso com pior h: O(nlog^2n)
+    Melhor caso com melhor h: O(nlogn)
+    
+    Comparações proporcionais a N^3/2
+*/
+
+#define SIZE 5000
+
+void shell_sort(int *vet, int inicio, int fim){
+    int h = 1, i, j, chave;
+
+    while(h < (fim - inicio + 1)/3)
+        h = 3*h + 1;
+
+    while(h>=1){
+        for (i = inicio + h; i <= fim; i++){
+            chave = vet[i];
+            for (j = i; j>=inicio+h && chave < vet[j-h]; j-=h){
+                vet[j] = vet[j-h];
+            }
+            vet[j] = chave;
+            }
+            h = h/3;
+        } 
+}
+
+int main(){
+    srand(time(NULL));
+    int vetor[SIZE];
+
+    for (int i = 0; i < SIZE; i++) {
+        vetor[i] = rand() % 5000 + 1;
+    }
+    
+    shell_sort(vetor, 0, SIZE);
+
+    for (int i = 0; i < SIZE; i++){
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
